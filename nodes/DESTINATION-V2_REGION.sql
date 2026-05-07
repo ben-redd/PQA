e14a4bb6ff6e0c20f2ca8ba5bcf5941abd7c1c8b
ben <ben.redd@coalesce.io>
2026-05-06 20:30:28 -0400

    add V2 node type and node

---
+++ nodes/DESTINATION-V2_REGION.sql
@@ -0,0 +1,7 @@
+@id("f6a4c1da-fbc0-4bbc-9666-1b44b21a42e5")
+@nodeType("c296a4e0-575c-4e60-8162-0f2a87bb067a")
+SELECT
+     "R_REGIONKEY" AS "R_REGIONKEY",
+     "R_NAME" AS "R_NAME",
+     "R_COMMENT" AS "R_COMMENT"
+FROM {{ ref('SOURCE', 'REGION') }} "REGION"
